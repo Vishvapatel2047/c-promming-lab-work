5a2bae69739778ed39d4542eec44cae6bbf33211
Vishva patel <vsvptl2047@gmail.com>
2025-10-24 12:50:18 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n,i;
+    printf("enter numbers");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    printf("%d",fibonacci(i));
+    return 0;
+}
+int fibonacci(int n){
+    if(n==0)
+    return 0;
+    else if(n==1)
+    return 1;
+    else
+    return fibonacci(n-1)+fibonacci(n-2);
+}
